c19aac091b518c1bd410d9228ca6c953169d31af
Your Name <y.dream.nest@icloud.com>
2020-04-18 01:25:49 +0900

    Initialize Repository

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        playSound(soundName: sender.currentTitle!)
+        sender.alpha = 0.5
+        print("Start")
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.2) {
+            print("End")
+            sender.alpha = 1
+        }
+    }
     
-
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
